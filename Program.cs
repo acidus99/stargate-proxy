de85de351c11e92e34c65596f455d9124c87bf4b
Acidus <acidus99@users.noreply.github.com>
2023-02-06 07:39:14 -0500

    host and port via command line args

--- Program.cs
+++ Program.cs
@@ -6,17 +6,19 @@ namespace Stargate
         static void Main(string[] args)
         {
 
-            if(args.Length != 2)
+            if(args.Length != 4)
             {
                 Console.WriteLine("Pass cert and key via args");
                 Environment.Exit(0);
             }
 
-            var cert = args[0];
-            var key = args[1];
+            var host = args[0];
+            var port = Convert.ToInt32(args[1]);
+            var cert = args[2];
+            var key = args[3];
 
-            App app = new App("localhost",
-                1994,
+            App app = new App(host,
+                port,
                 CertificateUtils.LoadCertificate(cert, key)
                 )
             {
