2ce856e035d754a48ae808ab095c2b602f69cc5d
Acidus <acidus99@users.noreply.github.com>
2023-02-24 09:24:03 -0500

    Use charset in headers/body to decode text

--- Transformers/HtmlTransformer.cs
+++ Transformers/HtmlTransformer.cs
@@ -1,37 +1,80 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
-using System.Net;
-using AngleSharp.Io;
-using CodeHollow.FeedReader;
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
 
 using HtmlToGmi;
+using HtmlToGmi.Encodings;
 using HtmlToGmi.Models;
+
 
 namespace Stargate.Transformers
 {
+
     public class HtmlTransformer : AbstractTextTransformer
     {
+
+        public HtmlTransformer()
+        {
+            //Ensure that extended codepages are supported
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
         public override bool CanTransform(string mimeType)
             => mimeType.StartsWith("text/html");
 
         public override SourceResponse Transform(Request request, SourceResponse response)
         {
-            var html = ReadAllText(response);
+
+            var responseBytes = ReadAllBytes(response);
+
 
             HtmlConverter converter = new HtmlConverter()
             {
                 AllowDuplicateLinks = true,
                 ShouldRenderHyperlinks = true
             };
-            var content = converter.Convert(request.Url.AbsoluteUri, html);
 
-            //reset my mime and body
-            response.Meta = "text/gemini";
-            response.Body = RenderToStream(content, html.Length);
+            ConvertedContent content = null;
+
+            //Do we have a charset in the Content-Type header? If so, that overrides all other charsets
+            if (HasCharset(response.SourceContentType))
+            {
+                string html = "";
+                try
+                {
+                    html = Encoding.GetEncoding(response.SourceContentType.CharSet).GetString(responseBytes);
+                }
+                catch (ArgumentException)
+                {
+                    throw new TransformationException($"unknown charset '{response.SourceContentType.CharSet}'");
+                }
+                content = converter.Convert(request.Url, html);
+            }
+            else
+            {
+                //check the HTML and use any charset there
+                HtmlDecoder htmlDecoder = new HtmlDecoder();
+                try
+                {
+                    htmlDecoder.Decode(responseBytes);
+                }
+                catch (ArgumentException)
+                {
+                    throw new TransformationException($"unknown charset '{htmlDecoder.SpecifiedCharset}'");
+                }
+                content = converter.Convert(request.Url, htmlDecoder.Document);
+            }
+
+            //whatever the charset was, it is now utf-8
+            response.Meta = $"text/gemini;charset=utf-8";
+            response.Body = RenderToStream(content, responseBytes.Length);
 
             return response;
         }
+
+        private bool HasCharset(MediaTypeHeaderValue contentType)
+            => !string.IsNullOrEmpty(contentType.CharSet);
 
         private MemoryStream RenderToStream(ConvertedContent content, int htmlLength)
         {
