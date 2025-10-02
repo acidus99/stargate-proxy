4549353d7e6ad9876e16df22be1c521d7c699599
Acidus <acidus99@users.noreply.github.com>
2025-10-01 20:05:58 -0400

    Fix: Detect bad charsets earlier, normalize quoted values
    Detect if a charset is supported or not earlier in the process, so we can return a 40 status code with an error. Also normalize quoted values before passing to .NET Encoding methods

---
+++ Helpers/MimeHelper.cs
@@ -0,0 +1,15 @@
+using System.Net.Http.Headers;
+
+namespace Stargate.Helpers;
+
+public static class MimeHelper
+{
+    public static string? NormalizeCharset(MediaTypeHeaderValue? contentType)
+    {
+        string charset = contentType?.CharSet ?? "";
+        // Trim whitespace and quotes some servers add around charset, which makes .NET's Encoding.GetEncoding throw an exception
+        charset = charset.Trim().Trim('"', '\'');
+        
+        return !string.IsNullOrWhiteSpace(charset) ? charset : null; 
+    }
+}
